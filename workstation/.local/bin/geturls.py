83efb9f0db0a0013217c9289326977e44b9234a8
Daniel M. Capella <polyzen@archlinux.org>
2020-06-12 02:19:37 -0400

    geturls: Add stdin support

--- workstation/.local/bin/geturls.py
+++ workstation/.local/bin/geturls.py
@@ -2,19 +2,25 @@
 """ Parse URLs from Firefox sessionstore JSON.
 Requires https://github.com/andikleen/lz4json.
 
-Example::
+Examples::
 
-    geturls.py <(lz4jsoncat ~/.mozilla/firefox/*.Default\
-            \ User/sessionstore-backups/recovery.jsonlz4) | less
+    lz4jsoncat recovery.jsonlz4 | geturls.py
+
+    geturls.py <(lz4jsoncat recovery.jsonlz4)
 """
 
+import fileinput
 import json
-import sys
 
-F = open(sys.argv[1], "r")
-JDATA = json.loads(F.read())
-F.close()
-for win in JDATA.get("windows"):
-    for tab in win.get("tabs"):
-        i = tab.get("index") - 1
-        print(tab.get("entries")[i].get("url"))
+
+def get_urls(line):
+    session = json.loads(line)
+    for win in session.get("windows"):
+        for tab in win.get("tabs"):
+            i = tab.get("index") - 1
+            url = tab.get("entries")[i].get("url")
+            print(url)
+
+
+for line in fileinput.input():
+    get_urls(line)
