4fd3572cb831b0c423391e28253526062a7b81b4
Daniel M. Capella <polyzen@archlinux.org>
2020-08-30 16:23:17 -0400

    geturls: Minor simplication to skirt warnings

--- workstation/.local/bin/geturls.py
+++ workstation/.local/bin/geturls.py
@@ -12,15 +12,10 @@ Examples::
 import fileinput
 import json
 
-
-def get_urls(line):
+for line in fileinput.input():
     session = json.loads(line)
     for win in session["windows"]:
         for tab in win["tabs"]:
             i = tab["index"] - 1
             url = tab["entries"][i]["url"]
             print(url)
-
-
-for line in fileinput.input():
-    get_urls(line)
