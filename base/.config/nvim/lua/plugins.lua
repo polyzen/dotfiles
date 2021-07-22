0c40f380018e27888f25c9e76c450ee54d409845
Daniel M. Capella <polyzen@archlinux.org>
2021-07-22 18:45:05 -0400

    Nvim: Indent Blankline: Show current context

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -79,6 +79,7 @@ return require('packer').startup(function()
       vim.g.indent_blankline_char_list = { '|', '¦', '┆', '┊' }
       vim.g.indent_blankline_filetype_exclude = { 'help' }
       vim.g.indent_blankline_buftype_exclude = { 'terminal' }
+      vim.g.indent_blankline_show_current_context = true
       vim.g.indent_blankline_use_treesitter = true
     end,
   }
