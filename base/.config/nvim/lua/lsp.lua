3aba32890b9d1b146ec926b64e80066198d046b5
Daniel M. Capella <polyzen@archlinux.org>
2021-08-08 00:02:31 -0400

    Nvim: LSP: Cleanup

--- base/.config/nvim/lua/lsp.lua
+++ base/.config/nvim/lua/lsp.lua
@@ -1,13 +1,16 @@
 local nvim_lsp = require('lspconfig')
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    'documentation',
-    'detail',
-    'additionalTextEdits',
-  },
-}
+nvim_lsp.util.default_config = vim.tbl_extend(
+  'force',
+  nvim_lsp.util.default_config,
+  {
+    flags = {
+      debounce_text_changes = 150,
+    },
+  }
+)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...)
     vim.api.nvim_buf_set_keymap(bufnr, ...)
@@ -16,13 +19,16 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, ...)
   end
 
+  -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
+  -- Mappings
   local opts = { noremap = true, silent = true }
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
@@ -47,38 +53,54 @@ local on_attach = function(client, bufnr)
 
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
+    vim.cmd([[
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]],
-      false
-    )
+    ]])
   end
+end
+
+local servers = { 'bashls', 'efm', 'pyright', 'tailwindcss', 'tsserver', 'yamlls' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup({ on_attach = on_attach })
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    'documentation',
+    'detail',
+    'additionalTextEdits',
+  },
+}
+
+local servers_with_snippets = { 'gopls', 'rust_analyzer' }
+for _, lsp in ipairs(servers_with_snippets) do
+  nvim_lsp[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
 end
 
 nvim_lsp.ccls.setup({
   on_attach = on_attach,
-  flags = {
-    debounce_text_changes = 150,
-  },
+  capabilities = capabilities,
   init_options = {
     highlight = {
       lsRanges = true,
     },
   },
-  capabilities = capabilities,
 })
 
 nvim_lsp.sumneko_lua.setup({
   on_attach = on_attach,
+  capabilities = capabilities,
   cmd = { 'lua-language-server' },
-  flags = {
-    debounce_text_changes = 150,
-  },
   settings = {
     Lua = {
       diagnostics = {
@@ -87,24 +109,3 @@ nvim_lsp.sumneko_lua.setup({
     },
   },
 })
-
-local servers = { 'bashls', 'efm', 'pyright', 'tailwindcss', 'tsserver', 'yamlls' }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup({
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    },
-  })
-end
-
-local servers_with_snippets = { 'gopls', 'rust_analyzer' }
-for _, lsp in ipairs(servers_with_snippets) do
-  nvim_lsp[lsp].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    flags = {
-      debounce_text_changes = 150,
-    },
-  })
-end
