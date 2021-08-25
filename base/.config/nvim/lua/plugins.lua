03145381a4d08efb6318082aa26b69a902c00212
Daniel M. Capella <polyzen@archlinux.org>
2021-08-25 05:06:48 -0400

    Nvim: cmp: Update bindings
    https://github.com/hrsh7th/nvim-cmp/commit/c0fcdd9c6196f1be94fe243f688e62611228d4bf

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -176,17 +176,17 @@ return require('packer').startup(function()
         local luasnip = require('luasnip')
         cmp.setup({
           mapping = {
-            ['<C-p>'] = cmp.mapping.prev_item(),
-            ['<C-n>'] = cmp.mapping.next_item(),
-            ['<C-d>'] = cmp.mapping.scroll(-4),
-            ['<C-f>'] = cmp.mapping.scroll(4),
+            ['<C-p>'] = cmp.mapping.select_prev_item(),
+            ['<C-n>'] = cmp.mapping.select_next_item(),
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.close(),
             ['<CR>'] = cmp.mapping.confirm({
               behavior = cmp.ConfirmBehavior.Insert,
               select = true,
             }),
-            ['<Tab>'] = cmp.mapping.mode({ 'i', 's' }, function(_, fallback)
+            ['<Tab>'] = function(fallback)
               if vim.fn.pumvisible() == 1 then
                 vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
               elseif luasnip.expand_or_jumpable() then
@@ -194,8 +194,8 @@ return require('packer').startup(function()
               else
                 fallback()
               end
-            end),
-            ['<S-Tab>'] = cmp.mapping.mode({ 'i', 's' }, function(_, fallback)
+            end,
+            ['<S-Tab>'] = function(fallback)
               if vim.fn.pumvisible() == 1 then
                 vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
               elseif luasnip.jumpable(-1) then
@@ -203,7 +203,7 @@ return require('packer').startup(function()
               else
                 fallback()
               end
-            end),
+            end,
           },
           snippet = {
             expand = function(args)
