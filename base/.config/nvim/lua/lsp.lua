34a4dada31a4e620b2535ab44f677d83e8f3b16a
Daniel M. Capella <polyzen@archlinux.org>
2021-08-25 16:01:29 -0400

    Nvim: LSP: Use update_capabilities helper function
    https://github.com/hrsh7th/cmp-nvim-lsp/commit/4ffde087495c9bb6cf908988b18bf9cdd473a6c0

--- base/.config/nvim/lua/lsp.lua
+++ base/.config/nvim/lua/lsp.lua
@@ -111,20 +111,7 @@ nvim_lsp.tsserver.setup({
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.preselectSupport = true
-capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    'documentation',
-    'detail',
-    'additionalTextEdits',
-  },
-}
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local servers_with_snippets = { 'gopls', 'rust_analyzer' }
 for _, lsp in ipairs(servers_with_snippets) do
