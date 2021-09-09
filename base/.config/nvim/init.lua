f9aafe552aa983ec19a2b3e2eab1795073ba8397
Daniel M. Capella <polyzen@archlinux.org>
2021-09-09 14:29:46 -0400

    Nvim: autoindent is enabled by default
    https://github.com/neovim/neovim/commit/8e3f67f2a0166994925af75c0efa972d76ea82de

--- base/.config/nvim/init.lua
+++ base/.config/nvim/init.lua
@@ -12,7 +12,6 @@ vim.opt.ignorecase = true -- Ignore case
 vim.opt.smartcase = true -- .. unless "pattern contains upper case characters"
 
 -- Two-space indentation
-vim.opt.autoindent = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
