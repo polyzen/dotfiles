f89a7135db866f1a70512464d8d29b0e7cd2cf69
Daniel M. Capella <polyzen@archlinux.org>
2022-01-08 04:40:26 -0500

    Nvim: Match docs and preferred style

--- base/.config/nvim/init.lua
+++ base/.config/nvim/init.lua
@@ -19,7 +19,7 @@ vim.opt.softtabstop = 2
 vim.cmd([[
   augroup highlight_yank
     autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank {}
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
   augroup END
 ]])
 
