58a7bf4b99d943fc0667b7e92dbc847310934101
Daniel M. Capella <polyzen@archlinux.org>
2022-01-08 13:55:08 -0500

    Nvim: gruvbox.nvim no longer depends on Lush
    https://github.com/ellisonleao/gruvbox.nvim/commit/6a0ccb52a4bb1256b50399bcd977920f63fefeec

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -40,7 +40,6 @@ return require('packer').startup(function()
   use('Konfekt/FastFold')
   use({
     'npxbr/gruvbox.nvim',
-    requires = { 'rktjmp/lush.nvim' },
     config = function()
       vim.g.gruvbox_italic = 1
       local h = tonumber(os.date('%H'))
