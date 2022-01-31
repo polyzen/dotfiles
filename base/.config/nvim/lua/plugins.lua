e771b0d333b0e946b895c19e2c72e56cfba4ea8a
Daniel M. Capella <polyzen@archlinux.org>
2022-01-30 23:10:52 -0500

    Nvim: StyLua pass

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -261,13 +261,13 @@ return require('packer').startup(function()
           require('gitsigns').setup({
             on_attach = function(bufnr)
               local function map(mode, lhs, rhs, opts)
-                  opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
-                  vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+                opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
+                vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
               end
 
               -- Navigation
-              map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
-              map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+              map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+              map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
 
               -- Actions
               map('n', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
@@ -287,7 +287,7 @@ return require('packer').startup(function()
               -- Text object
               map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
               map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-            end
+            end,
           })
         end,
       },
