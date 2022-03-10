c6b61ca13c43a5708c7776723e47b61f95dacb24
Daniel M. Capella <polyzen@archlinux.org>
2022-03-10 15:23:11 -0500

    Nvim: null-ls: Add rstcheck

--- base/.config/nvim/lua/lsp.lua
+++ base/.config/nvim/lua/lsp.lua
@@ -62,6 +62,7 @@ local sources = {
   }),
   null_ls.builtins.diagnostics.eslint,
   null_ls.builtins.diagnostics.flake8,
+  null_ls.builtins.diagnostics.rstcheck,
   null_ls.builtins.diagnostics.selene.with({
     cwd = function(params)
       local conf_file = vim.fn.findfile('selene.toml', params.root)
