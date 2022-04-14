318d7b9b989f888c4db6139e9beafe706d817ff3
Daniel M. Capella <polyzen@archlinux.org>
2022-04-13 23:17:51 -0400

    Nvim: cmp: Update bindings
    https://github.com/hrsh7th/nvim-cmp/commit/93cf84f7deb2bdb640ffbb1d2f8d6d412a7aa558

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -201,15 +201,11 @@ return require('packer').startup(function()
               luasnip.lsp_expand(args.body)
             end,
           },
-          mapping = {
-            ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-            ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-            ['<C-y>'] = cmp.config.disable,
-            ['<C-e>'] = cmp.mapping({
-              i = cmp.mapping.abort(),
-              c = cmp.mapping.close(),
-            }),
+          mapping = cmp.mapping.preset.insert({
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
             ['<CR>'] = cmp.mapping.confirm({ select = true }),
             ['<Tab>'] = cmp.mapping(function(fallback)
               if cmp.visible() then
@@ -231,7 +227,7 @@ return require('packer').startup(function()
                 fallback()
               end
             end, { 'i', 's' }),
-          },
+          }),
           sources = {
             { name = 'nvim_lsp' },
             { name = 'luasnip' },
