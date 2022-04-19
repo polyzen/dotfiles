2ef535d902ae8a083cceace670d298de2b62093f
Daniel M. Capella <polyzen@archlinux.org>
2022-04-19 01:55:00 -0400

    Nvim: cmp: Remove Super-Tab like mapping
    https://github.com/hrsh7th/nvim-cmp/issues/898#issuecomment-1100033382

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -189,18 +189,12 @@ return require('packer').startup(function()
         'hrsh7th/cmp-path',
       },
       config = function()
-        local has_words_before = function()
-          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-        end
-
         local cmp = require('cmp')
-        local luasnip = require('luasnip')
 
         cmp.setup({
           snippet = {
             expand = function(args)
-              luasnip.lsp_expand(args.body)
+              require('luasnip').lsp_expand(args.body)
             end,
           },
           mapping = cmp.mapping.preset.insert({
@@ -209,26 +203,6 @@ return require('packer').startup(function()
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.abort(),
             ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            ['<Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-              elseif has_words_before() then
-                cmp.complete()
-              else
-                fallback()
-              end
-            end, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item()
-              elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-              else
-                fallback()
-              end
-            end, { 'i', 's' }),
           }),
           sources = {
             { name = 'nvim_lsp' },
