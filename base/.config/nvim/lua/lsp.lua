9dbc7f78320272d8e546dd586a48459259f5473d
Daniel M. Capella <polyzen@archlinux.org>
2022-05-10 17:37:47 -0400

    Nvim: lsp_document_highlight: Clear only buflocal autocmds

--- base/.config/nvim/lua/lsp.lua
+++ base/.config/nvim/lua/lsp.lua
@@ -31,7 +31,13 @@ local on_attach = function(client, bufnr)
 
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_create_augroup('lsp_document_highlight', {})
+    vim.api.nvim_create_augroup('lsp_document_highlight', {
+      clear = false
+    })
+    vim.api.nvim_clear_autocmds({
+      buffer = bufnr,
+      group = 'lsp_document_highlight',
+    })
     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
       group = 'lsp_document_highlight',
       buffer = bufnr,
