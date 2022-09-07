8a16abafc44ea4b89624cc121514ae5f9c06439a
Daniel M. Capella <polyzen@archlinux.org>
2022-09-06 23:54:03 -0400

    Nvim: Setup nvim-tree
    https://github.com/kyazdani42/nvim-tree.lua/commit/a864b80bafeba6ea17d1a55e4c6f5e73f189f6bb

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -140,6 +140,7 @@ return require('packer').startup(function()
   use({
     'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = require('nvim-tree').setup(),
   })
   use({
     'nvim-treesitter/nvim-treesitter',
