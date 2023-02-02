fa5b6608cf9b873e5a6d986638fa8e48e496f063
Daniel M. Capella <polyzen@archlinux.org>
2023-02-02 13:07:07 -0500

    Nvim: nvim-tree: Open for directories and change directory
    https://github.com/nvim-tree/nvim-tree.lua/pull/1951

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -178,7 +178,19 @@ require('lazy').setup({
   {
     'nvim-tree/nvim-tree.lua',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = true,
+    config = function()
+      require('nvim-tree').setup()
+      local function open_nvim_tree(data)
+        local directory = vim.fn.isdirectory(data.file) == 1
+        if not directory then
+          return
+        end
+        vim.cmd.cd(data.file)
+        require('nvim-tree.api').tree.open()
+      end
+
+      vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
+    end,
   },
   {
     'nvim-treesitter/nvim-treesitter',
