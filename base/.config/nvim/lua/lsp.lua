e73abcb3c137beb211dffb60ad7480ecc49a4309
Daniel M. Capella <polyzen@archlinux.org>
2023-06-15 02:47:38 -0400

    Nvim: LSP: Remove format filter
    This doesn't seem to work properly when multiple clients are attached to
    the same buffer, even when using proper Lua (`return client.name ~=
    'lua_ls' and client.name ~= 'tsserver'`).
    
    Seems the proper solution will come with dynamicRegistration:
    https://github.com/neovim/neovim/commit/ddd92a70d2aab5247895e89abaaa79c62ba7dbb4

--- base/.config/nvim/lua/lsp.lua
+++ base/.config/nvim/lua/lsp.lua
@@ -13,13 +13,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- LSP mappings
     local lsp_formatting = function()
-      vim.lsp.buf.format({
-        timeout_ms = 2000,
-        bufnr = bufnr,
-        filter = function()
-          return client.name ~= 'lua_ls' or 'tsserver'
-        end,
-      })
+      vim.lsp.buf.format({ timeout_ms = 2000, bufnr = bufnr })
     end
 
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
