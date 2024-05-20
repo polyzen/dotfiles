7781f738331d73e24ef044eddc53a8aaa8382ce6
Daniel M. Capella <polyzen@archlinux.org>
2024-05-19 21:09:57 -0400

    Nvim: Switch to Comment.nvim from nvim-comment

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -30,13 +30,13 @@ require('lazy').setup({
     },
   },
   {
-    'terrortylor/nvim-comment',
+    'numToStr/Comment.nvim',
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
     opts = {
-      hook = function()
-        require('ts_context_commentstring.internal').update_commentstring()
+      pre_hook = function ()
+        require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
       end,
     },
-    main = 'nvim_comment',
   },
   'romainl/vim-cool',
   {
