3bc672e079ac6d08ac4607bc7f9886a6a7aa36de
Daniel M. Capella <polyzen@archlinux.org>
2025-03-10 10:19:36 -0400

    Nvim: Telescope: Add bindings from readme

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -191,6 +191,13 @@ require('lazy').setup({
       { 'nvim-treesitter/nvim-treesitter', lazy = true },
       'nvim-tree/nvim-web-devicons',
     },
+    init = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    end,
     config = function()
       require('telescope').setup({
         extensions = {
