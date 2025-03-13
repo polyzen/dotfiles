2329ffe875246ef82045ce37352c04beb8a6e040
Daniel M. Capella <polyzen@archlinux.org>
2025-03-13 16:46:53 -0400

    Nvim: Touch up

--- base/.config/nvim/lua/plugins.lua
+++ base/.config/nvim/lua/plugins.lua
@@ -21,6 +21,65 @@ require('lazy').setup({
     },
   },
   {
+    'saghen/blink.cmp',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'brenoprata10/nvim-highlight-colors',
+    },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      appearance = {
+        use_nvim_cmp_as_default = false,
+      },
+      completion = {
+        menu = {
+          draw = {
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local icon = ctx.kind_icon
+                  if ctx.item.source_name == 'LSP' then
+                    local color_item =
+                      require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr then
+                      icon = color_item.abbr
+                    end
+                  end
+                  return icon .. ctx.icon_gap
+                end,
+                highlight = function(ctx)
+                  local highlight = 'BlinkCmpKind' .. ctx.kind
+                  if ctx.item.source_name == 'LSP' then
+                    local color_item =
+                      require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr_hl_group then
+                      highlight = color_item.abbr_hl_group
+                    end
+                  end
+                  return highlight
+                end,
+              },
+            },
+          },
+        },
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      },
+      fuzzy = { implementation = 'lua' },
+      sources = {
+        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            score_offset = 100,
+          },
+        },
+      },
+      signature = { enabled = true },
+    },
+  },
+  {
     'stevearc/conform.nvim',
     cmd = { 'ConformInfo' },
     keys = {
@@ -352,65 +411,6 @@ require('lazy').setup({
       vim.g['zoom#statustext'] = '🔍 '
     end,
   },
-
-  -- Completions
-  {
-    'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      appearance = {
-        use_nvim_cmp_as_default = false,
-      },
-      completion = {
-        menu = {
-          draw = {
-            components = {
-              kind_icon = {
-                text = function(ctx)
-                  local icon = ctx.kind_icon
-                  if ctx.item.source_name == 'LSP' then
-                    local color_item =
-                      require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
-                    if color_item and color_item.abbr then
-                      icon = color_item.abbr
-                    end
-                  end
-                  return icon .. ctx.icon_gap
-                end,
-                highlight = function(ctx)
-                  local highlight = 'BlinkCmpKind' .. ctx.kind
-                  if ctx.item.source_name == 'LSP' then
-                    local color_item =
-                      require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
-                    if color_item and color_item.abbr_hl_group then
-                      highlight = color_item.abbr_hl_group
-                    end
-                  end
-                  return highlight
-                end,
-              },
-            },
-          },
-        },
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
-      },
-      fuzzy = { implementation = 'lua' },
-      sources = {
-        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
-        providers = {
-          lazydev = {
-            name = 'LazyDev',
-            module = 'lazydev.integrations.blink',
-            score_offset = 100,
-          },
-        },
-      },
-      signature = { enabled = true },
-    },
-  },
-  'rafamadriz/friendly-snippets',
 
   -- Git
   'hotwatermorning/auto-git-diff',
